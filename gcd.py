247a93e052fde5032754c041995693cb3fdafb2e
Rohan Arni <rohan.arni@gmail.com>
2018-03-04 18:06:31 -0500

    Minor professional tweaks. added a print statement when u launch the program. added a time sleep when its calculating.

--- gcd.py
+++ gcd.py
@@ -1,6 +1,8 @@
 '''check older version to put the 2 numbers that you want to find the gcd of in the command prompt like 'python3 gcd.py 5 15'
 thanks'''
-'''sub to my youtube channel at https://www.youtube.com/channel/UC-xwmwaB2jXQaLf6iM1zWFw?view_as=subscriber. I have no vids but i have good playlists if anyone watches ninjago or plays minecraft :)'''
+'''sub to my youtube channel at https://www.youtube.com/channel/UC-xwmwaB2jXQaLf6iM1zWFw?view_as=subscriber.
+I have no vids but i have good playlists if anyone watches ninjago or plays minecraft :)'''
+import time
 L1 = []
 L2 = []
 #get the divisors for a number
@@ -9,10 +11,8 @@ def get_factors(num_input):
     counter = 1
     while counter <= int(num_input):
         r = int(num_input) % counter
-
         if r == 0:
             tempList.append(counter)
-
         counter+=1
     return tempList
 def find_GCD(L1, L2):
@@ -20,15 +20,17 @@ def find_GCD(L1, L2):
     L3 = list(gcdset)
     L3.sort()
     return L3[-1]
+print("Enter 2 numbers and get their GCD! press CTRL + C to exit. (Note to Mac users: it's still control.) By Rohan Arni")
+while True:
+    num1 = input("Enter your first number here: ")
+    num2 = input("Enter your second number here: ")
 
-num1 = input("Enter your first number here: ")
-num2 = input("Enter your second number here: ")
+    L1=get_factors(num1)
 
-L1=get_factors(num1)
-#print (str(L1))
-L2=get_factors(num2)
-#print (str(L2))
+    L2=get_factors(num2)
 
-gcd = find_GCD(L1, L2)
 
-print ("The GCD of "+str(num1)+" and "+str(num2)+" is "+str(gcd))
+    gcd = find_GCD(L1, L2)
+    print("Calculating........")
+    time.sleep(3)
+    print ("The GCD of "+str(num1)+" and "+str(num2)+" is "+str(gcd) + "!")
