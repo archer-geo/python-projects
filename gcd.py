a06fae6368158d5ba26a933c41503cf0c3f5f888
Rohan Arni <rohan.arni@gmail.com>
2018-03-04 17:42:57 -0500

    added comments at the top. got rid of putting arguments in the command line and added an input in the program itself

--- gcd.py
+++ gcd.py
@@ -1,13 +1,14 @@
-
-import sys
+'''check older version to put the 2 numbers that you want to find the gcd of in the command prompt like 'python3 gcd.py 5 15'
+thanks'''
+'''sub to my youtube channel at https://www.youtube.com/channel/UC-xwmwaB2jXQaLf6iM1zWFw?view_as=subscriber. I have no vids but i have good playlists if anyone watches ninjago or plays minecraft :)'''
 L1 = []
 L2 = []
 #get the divisors for a number
 def get_factors(num_input):
     tempList=[]
     counter = 1
-    while counter <= num_input:
-        r = num_input % counter
+    while counter <= int(num_input):
+        r = int(num_input) % counter
 
         if r == 0:
             tempList.append(counter)
@@ -17,11 +18,11 @@ def get_factors(num_input):
 def find_GCD(L1, L2):
     gcdset = set(L1) & set(L2)
     L3 = list(gcdset)
-    L3.sort() 
+    L3.sort()
     return L3[-1]
 
-num1=int(sys.argv[1])
-num2=int(sys.argv[2])
+num1 = input("Enter your first number here: ")
+num2 = input("Enter your second number here: ")
 
 L1=get_factors(num1)
 #print (str(L1))
