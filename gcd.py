29f37a86d09f1de2738066155d547b3b645ae883
Rohan Arni <rohan.arni@gmail.com>
2018-03-04 16:42:33 -0500

    added factors func

--- gcd.py
+++ gcd.py
@@ -1,1 +1,28 @@
-#i
+L1 = []
+L2 = []
+L3 = []
+num1=6
+num2=36
+
+#get the divisors for a number
+def get_factors(num_input):
+    tempList=[]
+    counter = 1
+    while counter <= num_input:
+        #print("This is working inside while loop")
+        r = num_input % counter
+
+        if r == 0:
+            #print("This is working inside if loop")
+            tempList.append(counter)
+
+        counter+=1
+    return tempList
+
+
+#print ("checking factors for "+str(num1))
+L1=get_factors(num1)
+L2=get_factors(num2)
+#print('out of func all good')
+print(str(L1))
+print(str(L2))
