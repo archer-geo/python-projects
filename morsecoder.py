fc596ecab3cbdf313007d5f310fd5f2f4b0723ff
Rohan Arni <rohan.arni@gmail.com>
2018-03-10 23:06:57 -0500

    added input instead of preloaded keys. added while loop and print statement.

--- morsecoder.py
+++ morsecoder.py
@@ -1,11 +1,15 @@
-msg = 'this is working'
-morse_code_dict_key_find = ['a', 'b', 'c']
-morse_code_dict = {'a':'.-', 'b':'-...', 'c':'-.-.'}
-len_list = len(morse_code_dict_key_find)
+while True:
+    print("Write a sentence in lowercase and see it printed letter by letter! by Rohan Arni. Read readme.txt to see how to exit the program.")
+    linput=input("Enter a sentence here:")
+    morse_code_dict_key_find = list(linput)
+    morse_code_dict = {'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..', 'e':'.', 'f':'..-.', 'g':'--.', 'h':'....', 'i':'..', 'j':'.---', 'k':'-.-',
+    'l':'.-..', 'm':'--', 'n':'-.', 'o':'---', 'p':'.--.', 'q':'--.-', 'r':'.-.', 's':'...', 't':'-', 'u':'..-', 'v':'...-', 'w':'.--',
+    'x':'-..-', 'y':'-.--', 'z':'--..' }
+    len_list = len(morse_code_dict_key_find)
 
-counter = 0
+    counter = 0
 
-while counter < len_list:
-    findkey = morse_code_dict_key_find[counter]
-    print(morse_code_dict[findkey])
-    counter+=1
+    while counter < len_list:
+        findkey = morse_code_dict_key_find[counter]
+        print(morse_code_dict[findkey])
+        counter+=1
